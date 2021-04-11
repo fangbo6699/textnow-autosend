544dadbc00e574c072a256a9a41508fb27b0b83a
fangbo6699 <44827743+fangbo6699@users.noreply.github.com>
2021-04-10 22:38:17 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  username: process.env.TEXTNOW_USERNAME || "",
-  password: process.env.TEXTNOW_PASSWORD || "",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(726) 666-0002",
+  username: process.env.TEXTNOW_USERNAME || "zimeiti69@gmail.com",
+  password: process.env.TEXTNOW_PASSWORD || "A1G6K8MN",
+  recipient: process.env.TEXTNOW_RECIPIENT || "(323) 213-8699",
   message: process.env.TEXTNOW_MESSAGE || "autosend message",
 };
